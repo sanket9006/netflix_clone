bae5ac25c795ee9c7861e27be3099d9c579ca84b
Sanket Parshuram Patil <53385614+sanket9006@users.noreply.github.com>
2021-05-03 14:42:53 +0530

    Banner + CSS

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,25 +1,42 @@
 import axios from './Axios'
 import requests from './request'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+
+import './banner.css'
 
 export const Banner = () => {
 
+    const [movies, setMovies] = useState([]);
+
+    function truncate(str,n) {
+        return str?.length > n ? str.substr(0,n-1) + "..." : str  
+    }
+
     useEffect(() => {
-        
+
         async function fetchData() {
             const response = await axios.get(requests.fetchTrending)
-            console.log(response.data.results);
-
-            console.log(Math.floor(Math.random() * (response.data.results).length -1 )) ;
+            await setMovies(response.data.results[Math.floor(Math.random() * (response.data.results).length - 1)]);
         }
-        
+
         fetchData()
 
     }, [])
 
     return (
-        <div>
-            <h1>Hello</h1>
+        <div className="banner" style={{
+            backgroundImage: `url("https://image.tmdb.org/t/p/original${movies?.backdrop_path}")`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center center'
+        }}>
+        {console.log(movies)}
+
+            <div className="banner_contents">
+                <h2 className="banner_title">{movies?.title || movies?.original_title}</h2>
+                <button className="banner_btn">Play</button>
+                <button className="banner_btn">Watch Trailer</button>
+                <p className="bannner__description">{truncate(movies?.overview,150)}</p>
+            </div>
         </div>
     )
 }
