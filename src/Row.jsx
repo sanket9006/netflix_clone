99ff26f438785c8eaf2e2711ccc50508faedf3f8
Sanket Parshuram Patil <53385614+sanket9006@users.noreply.github.com>
2021-05-04 10:56:52 +0530

    Movie trailer

--- src/Row.jsx
+++ src/Row.jsx
@@ -1,4 +1,5 @@
 import Youtube from 'react-youtube'
+import movieTrailer  from 'movie-trailer'
 import React, { useEffect, useState } from "react";
 import axios from "./Axios";
 import './row.css'
@@ -6,6 +7,7 @@ const baseURL = "https://image.tmdb.org/t/p/original"
 
 function Row({ title, fetchURL, isbigger, islarger }) {
   const [movies, setMovies] = useState([]);
+  const [movieurl, setMovieurl] = useState([]);
 
   useEffect(() => {
     {
@@ -23,6 +25,19 @@ function Row({ title, fetchURL, isbigger, islarger }) {
     autoplay : 1,
   }
 
+  function youtubeplayit(movie) {
+
+    if(movieurl == "")
+    {
+      movieTrailer( movie?.title || movie?.original_title, {id: true, multi: false} )
+      .then( response => setMovieurl( response ) )
+      console.log({movie})
+    }else {
+      setMovieurl("")
+    }
+
+  }
+
   return (
     <div className="row">
       <h2>{title}</h2>
@@ -32,11 +47,15 @@ function Row({ title, fetchURL, isbigger, islarger }) {
           <img className={`poster 
                           ${isbigger ? "land_poster" : ""}                           
                           ${islarger && "land_larger"}`} 
-                src={`${baseURL}${isbigger ? movie.backdrop_path : movie.poster_path}`}>
+                          onClick={()=> {youtubeplayit(movie)}}
+                          src={`${baseURL}${isbigger ? movie.backdrop_path : movie.poster_path}`}>
                           </img>
         ))}
       </div>
-      <Youtube id="EYy02ZTmJ2I"  opts = {opts}/>
+
+      <div style={{marginTop:"20px",marginBottom:"20px"}}>
+      {movieurl && <Youtube videoId={movieurl} opts={opts}> </Youtube>}
+      </div>
     </div>
   );
 }
