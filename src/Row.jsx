8ea88dfc2e8dae1ced1b4bfe635666d7c0431da1
Sanket Parshuram Patil <53385614+sanket9006@users.noreply.github.com>
2021-05-06 07:50:58 +0530

    others

--- src/Row.jsx
+++ src/Row.jsx
@@ -1,5 +1,5 @@
 import Youtube from 'react-youtube'
-import movieTrailer  from 'movie-trailer'
+import movieTrailer from 'movie-trailer'
 import React, { useEffect, useState } from "react";
 import axios from "./Axios";
 import './row.css'
@@ -8,8 +8,11 @@ const baseURL = "https://image.tmdb.org/t/p/original"
 function Row({ title, fetchURL, isbigger, islarger }) {
   const [movies, setMovies] = useState([]);
   const [movieurl, setMovieurl] = useState([]);
+  const [oldmovieurl, setOldmovieurl] = useState([]);
 
   useEffect(() => {
+    setMovieurl("")
+
     {
       async function fetchData() {
         const response = await axios.get(`${fetchURL}`)
@@ -20,22 +23,19 @@ function Row({ title, fetchURL, isbigger, islarger }) {
   }, [fetchURL]);
 
   const opts = {
-    width:"100%",
+    width: "100%",
     height: "400px",
-    autoplay : 1,
+    autoplay: 1,
   }
 
   function youtubeplayit(movie) {
-
-    if(movieurl == "")
-    {
-      movieTrailer( movie?.title || movie?.original_title, {id: true, multi: false} )
-      .then( response => setMovieurl( response ) )
-      console.log({movie})
-    }else {
+    if (movieurl == "") {
+      movieTrailer(movie?.title || movie?.original_title, { id: true, multi: false })
+        .then(response => setMovieurl(response))
+      console.log({ movie })
+    } else {
       setMovieurl("")
     }
-
   }
 
   return (
@@ -46,15 +46,15 @@ function Row({ title, fetchURL, isbigger, islarger }) {
         {movies.map((movie) => (
           <img className={`poster 
                           ${isbigger ? "land_poster" : ""}                           
-                          ${islarger && "land_larger"}`} 
-                          onClick={()=> {youtubeplayit(movie)}}
-                          src={`${baseURL}${isbigger ? movie.backdrop_path : movie.poster_path}`}>
-                          </img>
+                          ${islarger && "land_larger"}`}
+            onClick={() => { youtubeplayit(movie) }}
+            src={`${baseURL}${isbigger ? movie.backdrop_path : movie.poster_path}`}>
+          </img>
         ))}
       </div>
 
-      <div style={{marginTop:"20px",marginBottom:"20px"}}>
-      {movieurl && <Youtube videoId={movieurl} opts={opts}> </Youtube>}
+      <div style={{ marginTop: "20px", marginBottom: "20px" }}>
+        {movieurl && <Youtube videoId={movieurl} opts={opts}> </Youtube>}
       </div>
     </div>
   );
